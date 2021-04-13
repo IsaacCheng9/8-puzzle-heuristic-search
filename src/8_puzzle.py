0d773c7e7b0305ac02e9f9070583f350088674bd
Isaac Cheng <isaaccheng9@gmail.com>
2021-04-13 01:28:20 +0100

    Refactor code

--- src/8_puzzle.py
+++ src/8_puzzle.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from copy import deepcopy
 
 import numpy as np
@@ -44,6 +45,17 @@ def assign_coordinates(board):
 
 
 def calculate_heuristic(heuristic, current, goal) -> int:
+    """
+    Calculates h(n) using either the Manhattan or Hamming Distance heuristic.
+
+    Args:
+        heuristic: The heuristic algorithm chosen by the user.
+        current: The current state of the board.
+        goal: The desired state of the board.
+
+    Returns:
+        The value of h(n) according to the heuristic algorithm used.
+    """
     if heuristic == "Manhattan":
         result = manhattan_heuristic(current, goal)
     else:
@@ -52,21 +64,50 @@ def calculate_heuristic(heuristic, current, goal) -> int:
 
 
 def manhattan_heuristic(current, goal) -> int:
+    """
+    Performs the Manhattan Distance heuristic.
+
+    Args:
+        current: The current state of the board.
+        goal: The desired state of the board.
+
+    Returns:
+        Minimum number of moves to reach the goal state from the current state.
+    """
     manhattan = abs(current // 3 - goal // 3) + abs(current % 3 - goal % 3)
     return sum(manhattan[1:])
 
 
 def hamming_heuristic(current, goal) -> int:
+    """
+    Performs the Hamming Distance heuristic.
+
+    Args:
+        current: The current state of the board.
+        goal: The desired state of the board.
+
+    Returns:
+        Number of misplaced tiles on the current state of the board.
+    """
     return np.sum(current != goal) - 1
 
 
 def generate_steps(state):
-    num_steps = np.array([], int).reshape(-1, 9)
+    """
+    Generates the step-by-step solution to reach the goal state.
+
+    Args:
+        state:
+
+    Returns:
+        The state of the board after each step leading to the goal state.
+    """
+    optimal = np.array([], int).reshape(-1, 9)
     last = len(state) - 1
     while last != -1:
-        num_steps = np.insert(num_steps, 0, state[last]["board"], 0)
+        optimal = np.insert(optimal, 0, state[last]["board"], 0)
         last = int(state[last]["parent"])
-    return num_steps.reshape(-1, 3, 3)
+    return optimal.reshape(-1, 3, 3)
 
 
 def search(heuristic, start, goal):
@@ -77,63 +118,70 @@ def search(heuristic, start, goal):
                      dtype=[("move", str, 1),
                             ("position", list),
                             ("delta", int)])
-    dt_state = [("board", list),
-                ("parent", int),
-                ("gn", int),
-                ("hn", int)]
+    state_record = [("board", list),
+                    ("parent", int),
+                    ("gn", int),
+                    ("hn", int)]
+    # Creates the priority queue.
+    dt_priority = [("position", int),
+                   ("f_function", int)]
 
+    previous_boards = defaultdict(bool)
     start_c = assign_coordinates(start)
     goal_c = assign_coordinates(goal)
     print(start_c)
     print(goal_c)
-    parent = -1
-    g_function = 0
-    h_function = calculate_heuristic(heuristic, start_c, goal_c)
-    state = np.array([(start, parent, g_function, h_function)],
-                     dt_state)
 
-    dt_priority = [("position", int),
-                   ("f_function", int)]
+    h_function = calculate_heuristic(heuristic, start_c, goal_c)
+    state = np.array([(start, -1, 0, h_function)],
+                     state_record)
     priority = np.array([(0, h_function)], dt_priority)
 
+    # Searches until the goal state is found.
     while True:
         # Sorts the priority queue.
         priority = np.sort(priority, kind="mergesort",
                            order=["f_function", "position"])
         # Explores the first node from the priority queue, and removes it from
         # the priority queue.
-        position, f_function = priority[0]
+        position = priority[0][0]
         priority = np.delete(priority, 0, 0)
-        board, parent, g_function, h_function = state[position]
-        board = np.array(board)
+        board = state[position][0]
+        g_function = state[position][2] + 1
         location = int(np.where(board == 0)[0])
-        g_function += 1
 
-        for m in moves:
-            if location not in m["position"]:
+        for move in moves:
+            if location not in move["position"]:
                 # Copies the current state of the board.
                 new_state = deepcopy(board)
                 # Performs the move.
-                new_state[location], new_state[location + m["delta"]] = \
-                    new_state[location + m["delta"]], new_state[location]
-                if not (np.all(list(state["board"]) == new_state, 1)).any():
-                    h_function = calculate_heuristic(
-                        heuristic, assign_coordinates(new_state), goal_c)
-                    # Generates and adds the new state to the queue.
-                    queue = np.array(
-                        [(new_state, position, g_function, h_function)],
-                        dt_state)
-                    state = np.append(state, queue, 0)
-                    f_function = g_function + h_function
-                    # Adds the new state to the priority queue.
-                    queue = np.array([(len(state) - 1, f_function)],
-                                     dt_priority)
-                    priority = np.append(priority, queue, 0)
+                delta_location = location + move["delta"]
+                new_state[location], new_state[delta_location] = \
+                    new_state[delta_location], new_state[location]
 
-                    # Stops the search if the goal state has been achieved.
-                    if np.array_equal(new_state, goal):
-                        print("Goal state has been achieved!")
-                        return state, len(priority)
+                # Ensures that boards aren't repeatedly processed.
+                if previous_boards[tuple(new_state)]:
+                    continue
+                previous_boards[tuple(new_state)] = True
+
+                h_function = calculate_heuristic(
+                    heuristic, assign_coordinates(new_state), goal_c)
+                # Generates and adds the new state to the queue.
+                queue = np.array(
+                    [(new_state, position, g_function, h_function)],
+                    state_record)
+                state = np.append(state, queue, 0)
+                # Calculates the total cost, and adds the new state to the
+                # priority queue.
+                f_function = g_function + h_function
+                queue = np.array([(len(state) - 1, f_function)], dt_priority)
+                priority = np.append(priority, queue, 0)
+
+                # Stops the search if the goal state has been achieved.
+                if np.array_equal(new_state, goal):
+                    print("\nGoal state has been achieved with the "
+                          "following steps:\n")
+                    return state, len(priority)
 
 
 def main():
@@ -147,8 +195,10 @@ def main():
           "\nGoal State:", goal)
 
     state, explored = search(heuristic, start, goal)
+    print(state)
     optimal = generate_steps(state)
-    print("Total States Generated:", len(state), "\nTotal States Explored:",
+    print(optimal)
+    print("\nTotal States Generated:", len(state), "\nTotal States Explored:",
           len(state) - explored, "\nTotal Steps for Optimal Solution:",
           len(optimal) - 1)
 
